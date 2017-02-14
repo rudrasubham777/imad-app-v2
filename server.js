7e63948e417e2f7deffef07ec0cee848b1b604d2
rudrasubham777 <rs9046496998@gmail.com>
2017-02-14 15:09:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function (req,res){
+    res.send('Article one responded and will be executed');
+    });
+    
+app.get('/article-one',function (req,res){
+    res.send('Article one responded and will be executed');
+    });
+    
+ app.get('/article-one',function (req,res){
+    res.send('Article one responded and will be executed');
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
