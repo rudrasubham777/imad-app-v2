1c02f873cea88ef7bb9cdcb27ee26d544fe13112
rudrasubham777 <rs9046496998@gmail.com>
2017-02-15 22:43:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,8 +87,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui' , 'index.html'));
 });
 
-app.get('/:articleName',function (req, res){
-    var articleName = req.params.articleName; 
+app.get('/:articleName', function (req, res) {
+    var articleName = req.parans.articleName; 
    res.send(createTemplate(articles[articleName]));
     });
     
