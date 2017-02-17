f99c10d38bda7f81c5ce4d60d9f8dfdee0a50e38
rudrasubham777 <rs9046496998@gmail.com>
2017-02-17 09:28:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ var htmlTemplate = `
 return htmlTemplate;
 }
 */
-app.get('/index.html', function (req, res) {
+app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui' , 'index.html'));
 });
 
