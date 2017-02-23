78578d036c5012bf991a0dd5016a675820ec1af7
rudrasubham777 <rs9046496998@gmail.com>
2017-02-23 21:27:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ element.innerHTML = 'new value';
 
 //move the image
 var img = document.getElementById('madi');
-img.onclick = function () {
-    img.style.marginLeft='100px';
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
+img.onclick = function () {
+    var interval = setInterval(moveRight , 100);
+};
