dd2bde23730bb8c5bf4ab5c236101cba125331bb
rudrasubham777 <rs9046496998@gmail.com>
2017-02-23 23:01:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,21 @@
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function()
-{
-    counter = counter+1;
-    var span = document.getElementById('count');
+{   
+    var request = new XMLHttpRequet();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLhttpRequest.DONE){
+            if (request.status === 200){
+                var counter = request.responseText;
+                 var span = document.getElementById('count');
     span.innerHTML = counter.toString();
+            }
+        }
+    };
+   
+   
+    request.open('GET' , 'http://rudrasubham777.imad.hasura-app.io/counter ',true);
+    request.send(null);
     
-    
-}
+};
