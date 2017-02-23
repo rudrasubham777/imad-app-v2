f49ce0a0c3207d1920e024f8ca7d129c03deeb3b
rudrasubham777 <rs9046496998@gmail.com>
2017-02-23 22:51:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,11 @@
-console.log('Loaded!');
-// cahnge the text here
-var element = document.getElementById('main-text');
-element.innerHTML = 'new value';
+var button = document.getElementById('counter');
+var counter = 0;
 
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+button.onclick = function()
+{
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+    
 }
-img.onclick = function () {
-    var interval = setInterval(moveRight , 10);
-};
